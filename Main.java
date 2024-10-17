980acf87db148e743817e57b98664c1f38fcde60
jers2305 <javierr23205@gmail.com>
2024-10-17 11:18:26 +0200

    conpilar argumentos

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public Main{
 public static void main(String[] args){
-	System.out.println("hola");
-
+	Try{
+	while(args.lenth!=0){
+	ArrayList<String> array=new ArrayList<String>();
+	array.add(args);
+	for(String arg:array){
+		System.out.println(arg);
+		}
+}
+		}catch(Exception e){
+		Ssystem.out.println("error: "+e);
+}
 	}
 }
