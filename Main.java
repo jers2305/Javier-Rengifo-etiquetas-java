013f17ab0690bdd672cf1391962149b03e44c746
jers2305 <javierr23205@gmail.com>
2024-10-24 09:35:15 +0200

    primer commit del ejercio del dado

--- Main.java
+++ Main.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ArrayList<String> array = new ArrayList<String>();
+        // Verificar que se ha pasado al menos un argumento
+        if (args.length != 0) {
+            // Obtener el número de caras del dado a partir del argumento
+            int numCaras = Integer.parseInt(args[0]);
 
-             if (args.length != 0) {
-                for (String arg : args) {
-                    array.add(arg);
-                }
-            }
+            // Generar un número aleatorio entre 1 y numCaras
+            Random random = new Random();
+            int resultado = random.nextInt(numCaras) + 1;
 
-             for (String arg : array) {
-                System.out.println(arg);
-            }
-
-        } catch (Exception e) {
-            System.out.println("error: " + e);
+            // Mostrar el resultado de la tirada
+            System.out.println("Tiraste un dado de " + numCaras + " caras y salió: " + resultado);
+        } else {
+            System.out.println("Debes proporcionar el número de caras del dado.");
         }
     }
 }
