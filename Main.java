96d1107f4c72c208e55ec69bbc346ae7c964d6e0
jers2305 <javierr23205@gmail.com>
2024-10-24 09:33:00 +0200

    primera version

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
-public Main{
-public static void main(String[] args){
-	Try{
-	while(args.lenth!=0){
-	ArrayList<String> array=new ArrayList<String>();
-	array.add(args);
-	for(String arg:array){
-		System.out.println(arg);
-		}
-}
-		}catch(Exception e){
-		Ssystem.out.println("error: "+e);
-}
-	}
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> array = new ArrayList<String>();
+
+             if (args.length != 0) {
+                for (String arg : args) {
+                    array.add(arg);
+                }
+            }
+
+             for (String arg : array) {
+                System.out.println(arg);
+            }
+
+        } catch (Exception e) {
+            System.out.println("error: " + e);
+        }
+    }
 }
